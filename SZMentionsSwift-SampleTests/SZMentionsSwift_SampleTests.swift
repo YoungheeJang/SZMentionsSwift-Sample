e292aeaa160472cdcfb16019b84329131cf69547
YH.Jang <youngheejang@gmail.com>
2019-01-03 10:42:45 +0900

    add new project

---
+++ SZMentionsSwift-SampleTests/SZMentionsSwift_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SZMentionsSwift_SampleTests.swift
+//  SZMentionsSwift-SampleTests
+//
+//  Created by YH.Jang on 03/01/2019.
+//  Copyright © 2019 YH.Jang. All rights reserved.
+//
+
+import XCTest
+@testable import SZMentionsSwift_Sample
+
+class SZMentionsSwift_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
